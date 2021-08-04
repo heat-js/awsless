4aae6588c1c1916c4ff9213878dab3811bce81c8
Michel Monen <michel@monen.com>
2021-08-04 18:22:44 +0200

    v0.1.43

--- src/feature/cloud-front/build.js
+++ src/feature/cloud-front/build.js
@@ -1,28 +1,28 @@
 
 const { rollup } 	= require('rollup');
-const { getBabelOutputPlugin } = require('@rollup/plugin-babel');
+const { babel, getBabelOutputPlugin } 	= require('@rollup/plugin-babel');
 const { terser } 	= require('rollup-plugin-terser');
 const coffeescript 	= require('rollup-plugin-coffee-script');
 const commonjs 		= require('rollup-plugin-commonjs');
 const nodeResolve 	= require('rollup-plugin-node-resolve');
 const { expose } 	= require('threads/worker');
-// const builtins 		= require('builtin-modules');
+const builtins 		= require('builtin-modules');
 
 expose({
 	build: async function(inputFile) {
 		const bundle = await rollup({
 			input: inputFile,
-			// external: builtins,
-			// onwarn: function (message) {
-			// 	supressedWarnings = [
-			// 		'MISSING_GLOBAL_NAME',
-			// 		'MISSING_NODE_BUILTINS'
-			// 	]
+			external: builtins,
+			onwarn: function (message) {
+				supressedWarnings = [
+					'MISSING_GLOBAL_NAME',
+					'MISSING_NODE_BUILTINS'
+				]
 
-			// 	if (!supressedWarnings.includes(message.code)) {
-			// 		console.error(message.toString());
-			// 	}
-			// },
+				if (!supressedWarnings.includes(message.code)) {
+					console.error(message.toString());
+				}
+			},
 			plugins: [
 				coffeescript(),
 				nodeResolve({
@@ -34,7 +34,15 @@ expose({
 					allowAllFormats: true,
 					presets: [ "@babel/preset-env" ]
 				}),
-				terser(),
+				terser({
+					compress: {
+						negate_iife: false,
+						defaults: false,
+						ecma: 5,
+					},
+					mangle: true,
+					toplevel: false,
+				}),
 			]
 		});
 
